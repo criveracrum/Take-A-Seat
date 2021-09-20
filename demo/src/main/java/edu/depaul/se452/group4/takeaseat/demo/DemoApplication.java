852359f381be79cb1c7aaed0ea0e34e8b2055825
Chadwick Rivera-Crum <criveracrum@gmail.com>
2021-09-19 19:09:00 -0500

    Initialization of Spring boot/gradle project

---
+++ demo/src/main/java/edu/depaul/se452/group4/takeaseat/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package edu.depaul.se452.group4.takeaseat.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/edu/depaul/se452/group4/takeaseat/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.depaul.se452.group4.takeaseat.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
