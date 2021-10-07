e0899bea6adc88b52948f70d58f5ea526071d93c
Chadwick Rivera-Crum <criveracrum@gmail.com>
2021-10-07 14:07:21 -0500

    Docker/DB initialization with setup of properties for dev

--- demo/src/main/java/edu/depaul/se452/group4/takeaseat/demo/DemoApplication.java
+++ demo/src/main/java/edu/depaul/se452/group4/takeaseat/demo/DemoApplication.java
@@ -1,13 +1,28 @@
 package edu.depaul.se452.group4.takeaseat.demo;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class DemoApplication {
+	@Value( "${app.greeting}" )
+    private String greeting;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+	@Bean
+    public CommandLineRunner showMessage() {
+      return (args) -> {
+        System.out.println("begin encoded");
+        System.out.println(greeting);
+              System.out.println("end encoded");
+      };
+    }    
 
 }
