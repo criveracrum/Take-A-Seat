882d2b69071934ed110d415157c33b2bfda447a7
Unsagiro <aldoabz92@gmail.com>
2021-10-09 18:33:35 -0500

    SQL writes!

--- demo/src/main/java/edu/depaul/se452/group4/takeaseat/demo/persistence/relational/Amenities/AmenitiesRepository.jav
+++ demo/src/main/java/edu/depaul/se452/group4/takeaseat/demo/persistence/relational/Amenities/AmenitiesRepository.jav
@@ -4,8 +4,6 @@ package edu.depaul.se452.group4.takeaseat.demo.persistence.relational.Amenities;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AmenitiesRepository extends JpaRepository<Amenities,Long> {
-       
-    @Override
     
 
     }
--- demo/src/main/resources/schema.sql
+++ demo/src/main/resources/schema.sql
@@ -1,24 +1,28 @@
 
 
 
-
+DROP TABLE IF EXISTS  Amenities;
+DROP TABLE IF EXISTS  Workspace;
 
 
 
 
 CREATE TABLE Amenities(
-    amenityID  INT AUTO_INCREMENT PRIMARY KEY,
-    amenityName VARCHAR(10),
-    PRIMARY KEY (amenityID)
+    amenityID serial  PRIMARY KEY,
+    amenityName VARCHAR(10)
+    
 );
 
 
 CREATE TABLE workspace(
-    workspaceID INT AUTO_INCREMENT PRIMARY KEY,
+    
+    workspaceID serial  PRIMARY KEY,
     workspacename VARCHAR(10),
-    amenityID INT FOREIGN KEY(amenityID.amenities),
-    maxSize INT NOT NULL,
-    PRIMARY KEY (workspaceID)
+    amenityID  INT,
+    maxSize INT NOT NULL
+    
+    
+    
 );
 
 
