77ed23954cbe29cf98932b57c02d22b0f4d14ed4
Chadwick Rivera-Crum <criveracrum@gmail.com>
2021-10-10 17:57:29 -0500

    Updated to using MongoRepo

--- demo/src/main/java/edu/depaul/se452/group4/takeaseat/demo/reservation/NewsfeedRepository.java
+++ demo/src/main/java/edu/depaul/se452/group4/takeaseat/demo/reservation/NewsfeedRepository.java
@@ -2,9 +2,9 @@ package edu.depaul.se452.group4.takeaseat.demo.reservation;
 
 import java.util.List;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface NewsfeedRepository extends CrudRepository<Newsfeed, String>{
+public interface NewsfeedRepository extends MongoRepository<Newsfeed, String>{
 
     @Override
     List<Newsfeed> findAll();
