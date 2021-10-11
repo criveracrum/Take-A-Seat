63452e882b87c59b0384fe5bc993fe0e92689405
Chadwick Rivera-Crum <criveracrum@gmail.com>
2021-10-10 20:01:26 -0500

    fixed folder names

--- demo/src/main/java/edu/depaul/se452/group4/takeaseat/demo/Workspace/WorkspaceRepository.java
+++ demo/src/main/java/edu/depaul/se452/group4/takeaseat/demo/Workspace/WorkspaceRepository.java
@@ -1,4 +1,4 @@
-package edu.depaul.se452.group4.takeaseat.demo.Workspace;
+package edu.depaul.se452.group4.takeaseat.demo.workspace;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
