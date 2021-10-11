6aa02b8d285dc5ada3a959219ed47149cddeb28a
aza <azaleamendoza@protonmail.com>
2021-10-11 13:31:52 -0500

    combined the two sql files into one, all things run

--- demo/src/main/resources/schema-postgresql.sql
+++ demo/src/main/resources/schema-postgresql.sql
@@ -2,6 +2,10 @@ DROP TABLE IF EXISTS reservations;
 DROP TABLE IF EXISTS spaces;
 DROP TABLE IF EXISTS building;
 DROP TABLE IF EXISTS prices;
+DROP TABLE IF EXISTS Amenities;
+DROP TABLE IF EXISTS workspace;
+DROP TABLE IF EXISTS Employee;
+DROP TABLE IF EXISTS Team;
 CREATE TABLE reservations (
     id serial PRIMARY KEY NOT NULL,
     reservation_num varchar(10),
@@ -22,3 +26,23 @@ CREATE TABLE prices (
     id serial PRIMARY KEY NOT NULL,
     price SMALLINT
 );
+CREATE TABLE Amenities(
+    amenityID serial PRIMARY KEY,
+    amenityName VARCHAR(10)
+);
+CREATE TABLE workspace(
+    workspaceID serial PRIMARY KEY,
+    workspacename VARCHAR(10),
+    amenityID INT,
+    maxSize INT NOT NULL
+);
+CREATE TABLE Employee(
+    id serial PRIMARY KEY,
+    fullname VARCHAR(255),
+    birthdate DATE
+);
+CREATE TABLE Team(
+    id serial PRIMARY KEY,
+    teamName VARCHAR(20),
+    employeeCount INT
+)
