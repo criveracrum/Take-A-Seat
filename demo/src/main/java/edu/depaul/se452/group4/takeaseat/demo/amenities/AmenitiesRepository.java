d69f487fb5509d29137c91165552445c83eb3885
Unsagiro <aldoabz92@gmail.com>
2021-10-27 15:01:59 -0500

    Fixed import on Amenities.java

--- demo/src/main/java/edu/depaul/se452/group4/takeaseat/demo/amenities/AmenitiesRepository.java
+++ demo/src/main/java/edu/depaul/se452/group4/takeaseat/demo/amenities/AmenitiesRepository.java
@@ -3,8 +3,11 @@ package edu.depaul.se452.group4.takeaseat.demo.amenities;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AmenitiesRepository extends JpaRepository<Amenities,Long> {
     
-
+    @Override
+    List<Amenities> findAll();
     }
 
