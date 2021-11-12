5e910ab5c93d40434b4bc180283eed9c2aa40592
Chadwick Rivera-Crum <criveracrum@gmail.com>
2021-11-11 18:18:13 -0600

    update logic so that newsfeed matches reservations

--- demo/src/main/java/edu/depaul/se452/group4/takeaseat/demo/reservation/NewsfeedController.java
+++ demo/src/main/java/edu/depaul/se452/group4/takeaseat/demo/reservation/NewsfeedController.java
@@ -16,12 +16,30 @@ public class NewsfeedController {
     @Autowired
     private NewsfeedRepository newsfeedRepo;
 
+    @Autowired
+    private ReservationRepository reservationRepo;
+
     @GetMapping
     public String showNewsfeed(Model model) {
+        newsfeedRepo.deleteAll();
+        addToNewsFeed();
         List<Newsfeed> newsfeed=newsfeedRepo.findAll();
         Collections.reverse(newsfeed);
         model.addAttribute("newsfeed", newsfeed);
         return "newsfeed/newsfeed";
 
+    }
+
+    public void addToNewsFeed(){
+        List<Reservation> reservations = reservationRepo.findAll();
+        for (Reservation reservation : reservations) {
+            Newsfeed item = new Newsfeed();
+            item.setReservationDateTime(reservation.getReservationDateTime());
+            item.setCreatorID(reservation.getCreatorID());
+            item.setCreatedDateTime(reservation.getCreatedDateTime());
+            item.setReservationNum(reservation.getReservationNumber());
+            item.setWorkspaceType(reservation.getSpaceType());
+            newsfeedRepo.save(item);
+        }
     }
 }
