4396824ac89f537c254a428dd2eca0c6b887d534
aza <azaleamendoza@protonmail.com>
2021-11-14 16:44:33 -0600

    merge fix sorry

--- demo/src/main/resources/data.sql
+++ demo/src/main/resources/data.sql
@@ -1,48 +1,3 @@
-<< << << < HEAD
-INSERT INTO amenities (amenity_name)
-VALUES ('Coffee Machine'),
-    ('Espresso Machine'),
-    ('Microwave'),
-    ('Projector'),
-    ('Toaster'),
-    ('TV'),
-    ('Radio'),
-    ('Speakers');
-INSERT INTO workspace(workspacename, max_size)
-VALUES ('Desk', 1),
-    ('Desk', 1),
-    ('Desk', 1),
-    ('Desk', 1),
-    ('Desk', 1),
-    ('Booth', 4),
-    ('Cubicle', 1),
-    ('Conference Room', 20),
-    ('High Top', 4),
-    ('Double Desk', 2),
-    ('Meeting Room', 6);
-INSERT INTO employees(emp_id, fullname, birthdate)
-VALUES ('2', 'Tom Select', '1981-06-01'),
-    ('4', 'Johnny Appleseed', '1972-07-30');
-INSERT INTO team(id, teamname, employeecount)
-VALUES ('404', 'Team not Found', '5'),
-    ('007', 'The Bonds', '2'),
-    ('452', 'Enterprise Computers', '15');
-INSERT INTO reservations(
-        reservation_num,
-        res_date,
-        creator_id,
-        space_type,
-        space_id
-    )
-VALUES (
-        '11111',
-        '2021-10-19 12:00:00',
-        1,
-        'WORKSPACE',
-        1
-    ),
-    ('22222', '2021-10-31 13:30:00', 1, 'AMENITY', 2);
-== == == =
 INSERT INTO spaces(space_name, workspace_type)
 VALUES ('Coffee Machine', 'AMENITY'),
     ('Espresso Machine', 'AMENITY'),
@@ -97,6 +52,3 @@ INSERT INTO reservations(
     )
 VALUES ('11111', '2021-10-19 12:00:00', 1, 'AMENITY', 1),
     ('22222', '2021-10-31 13:30:00', 1, 'AMENITY', 2);
--- update spaces set reservation_list=(select reservation_num from reservations WHERE reservation_num='11111') WHERE space_id=1;
--- update spaces set reservation_list=(select reservation_num from reservations WHERE reservation_num='22222') WHERE space_id=2;
->> >> >> > 3f83068ed5982a8d6e230ef6c978a7ab52eb4f84
