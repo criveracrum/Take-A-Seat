9f4a6b5bb7ddd7534d60883b74bf0df036b481c6
Chadwick Rivera-Crum <criveracrum@gmail.com>
2021-11-16 12:25:47 -0600

    -added team reviews/controller

--- demo/src/main/java/edu/depaul/se452/group4/takeaseat/demo/team/TeamReviewController.java
+++ demo/src/main/java/edu/depaul/se452/group4/takeaseat/demo/team/TeamReviewController.java
@@ -3,26 +3,58 @@ package edu.depaul.se452.group4.takeaseat.demo.team;
 import java.util.Collections;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 @RequestMapping("teamreviews")
 public class TeamReviewController {
 
-  @Autowired
-  private TeamReviewRepository teamReviewRepo;
+    @Autowired
+    private TeamReviewRepository teamReviewRepo;
 
-  @GetMapping
-  public String showTeamReviews(Model model) {
+    @Autowired
+    private TeamService teamService;
 
-    List<TeamReview> reviews = teamReviewRepo.findAll();
-    Collections.reverse(reviews);
-    model.addAttribute("reviews", reviews);
-    return "team/reviews";
-
-  }
+    @GetMapping
+    public String showTeamReviews(Model model) {
+  
+      List<TeamReview> reviews = teamReviewRepo.findAll();
+      Collections.reverse(reviews);
+      model.addAttribute("reviews", reviews);
+      model.addAttribute("teams", teamService.findAll());
+      return "teamreviews/list-reviews";
+  
+    }
+    @GetMapping("/add")
+    public String addTeamReview(Model model) {
+      model.addAttribute("teamreview", new TeamReview());
+      model.addAttribute("teams", teamService.findAll());
+      return "teamreviews/add-teamreview";
+  
+    }
+    @PostMapping("/add")
+    public String createTeamReview(@Valid TeamReview review, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "teamreviews/add-teamreview";
+        }
+        
+        teamReviewRepo.save(review);
+      return "redirect:/teamreviews";
+  
+    }
+    @GetMapping("/delete/{team}")
+    public String delete(@PathVariable("team") String team, Model model) {
+        teamReviewRepo.deleteById(team);  
+        return "redirect:/teamreviews";
+    } 
 }
